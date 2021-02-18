fb0bada21b4157fc136b80d496a896b4fe4615cf
Cynmil <hyunkok1@uci.edu>
2021-02-17 22:20:57 -0800

    added styles to title and button

---
+++ pages/quiz.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
+import Link from 'next/link'
+
+const NavComponent = dynamic(() => import('../components/navigation'))
+
+export default function Quiz() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Journeymxn</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <NavComponent />
+      
+      <header className="home-header">
+        <div className="home-header__heading-container">
+            <h1 className="heading-primary">
+                <span className="heading-primary--main u-margin-bottom-small">Journeymxn</span>
+                <span className="heading-primary--sub">Discover Your</span>
+                <span className="heading-primary--sub">Colleges. Careers. Futures.</span>
+            </h1>
+
+            <Link href="/quiz">
+              <a href="#!" className="btn btn--white btn--animated">Take Our Career Quiz</a>
+            </Link>
+        </div>
+      </header>
+    </div>
+  )
+}
