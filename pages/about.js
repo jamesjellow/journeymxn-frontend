3bed2f107662c316aac27581e8ff64ba64b4d91b
Cynmil <hyunkok1@uci.edu>
2021-02-25 15:28:48 -0800

    added reponsiveness with mixns and meta tag

--- pages/about.js
+++ pages/about.js
@@ -4,23 +4,20 @@ import Link from 'next/link'
 
 const NavComponent = dynamic(() => import('../components/navigation'))
 
-export default function Quiz() {
+export default function About() {
   return (
     <div className="container">
       <Head>
-        <title>Journeymxn</title>
         <meta name="viewport" content="width=device-width, initial-scale=0.41, maximum-scale=1" />
+        <title>Journeymxn</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <NavComponent />
       
-      <div className="quiz">
-        <div className="quiz__container">
-          
-        </div>
+      <div>
+        <h2>About</h2>
       </div>
-      
     </div>
   )
 }
