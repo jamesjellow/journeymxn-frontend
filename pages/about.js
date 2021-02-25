de581c7185ecc413cb31a17f32a03042dc66cb29
Patrick Castillofuentes <ptcastil@uci.edu>
2021-02-25 13:46:01 -0800

    resized gabe

--- pages/about.js
+++ pages/about.js
@@ -22,17 +22,44 @@ export default function About() {
                 Journeymxn is a web-based platform that aggregates up-to-date labor trend data, vocational/trade/skill-based career programs, and matches student profiles that are generated after an assessment quiz with a comprehensive roadmap that can use to explore different options.
                 </p>
             </section>
-
             <section className="about-container__team">
+
                 <h2 className="team-title">
                     Meet the team
                 </h2>
+
                 <div className="sponsor">
-                    <img src="./public/icon-256.jpg"/>
+                    <img src="/Hai.jpg" alt="photo of hai" className="person-photo" />
+                    <p className="person-text">
+                      Hai Truong is an experience marketer, copywriter, and podcast producer, whose projects primarily focus on student success and teaching excellence.
+                    </p>
+
                 </div>
                 <ul className="team-developers">
-                    <div className="personOne">
-                    </div>
+                    <li className="personOne">
+                      <p className="person-text">
+                        Hai Truong is an experience marketer, copywriter, and podcast producer, whose projects primarily focus on student success and teaching excellence.
+                      </p>
+                      <img src="/Justin.jpg" alt="photo of hai" className="person-photo" />
+                    </li>
+                    <li className="personTwo">
+                      <img src="/Mugdha.jpg" alt="photo of hai" className="person-photo" />
+                      <p className="person-text">
+                        Hai Truong is an experience marketer, copywriter, and podcast producer, whose projects primarily focus on student success and teaching excellence.
+                      </p>
+                    </li>
+                    <li className="personThree">
+                      <img src="/Gabe.jpg" alt="photo of hai" className="person-photo" />
+                      <p className="person-text">
+                        Hai Truong is an experience marketer, copywriter, and podcast producer, whose projects primarily focus on student success and teaching excellence.
+                      </p>
+                    </li>
+                    <li className="personFour">
+                      <img src="/Patrick.jpg" alt="photo of hai" className="person-photo" />
+                      <p className="person-text">
+                        Hai Truong is an experience marketer, copywriter, and podcast producer, whose projects primarily focus on student success and teaching excellence.
+                      </p>
+                    </li>
                 </ul>
             </section>
         </div>
