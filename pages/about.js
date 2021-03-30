64540a325742363adda70e98cf6fb6d308148c12
Justin Kang <43480842+Cynmil@users.noreply.github.com>
2021-03-29 20:05:45 -0700

    About us (#13)
    * reformatted about page, updated dummy text, add styles
    
    * double check
    
    * Fixed responsiveness of the sponsor card
    
    * changed gradient color
    
    Co-authored-by: Patrick Castillofuentes <ptcastil@uci.edu>

--- pages/about.js
+++ pages/about.js
@@ -6,79 +6,80 @@ const NavComponent = dynamic(() => import('../components/navigation'))
 
 export default function About() {
   return (
-  <div>
-    <Head>
-      <title>Journeymxn</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    <div className="container">
+      <Head>
+        <title className="about_journey">Journeymxn</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <NavComponent />
 
-    <NavComponent />
+      <article className="about-container">
+        <section className="about__title-container">
+          <div className="title-pair">
+            <h1 className="title-text">Journeymxn: Data Analytics</h1>
+          </div>
+          <div className="title-info">
+            <h1 className="goal-title">Our Goal:</h1>
+            <p className="title-goal-text">Journeymxn is a web-based platform that aggregates up-to-date labor trend data, vocational/trade/skill-based career programs, and matches student profiles that are generated after an assessment quiz with a comprehensive roadmap that can use to explore different options. Many high school students do not know what to do after they finish their academic curriculums. With little resources and guidance, they often end up feeling lost or hopeless despite increasing job opportunities and a large number of 4-year universities.We believe this issue comes from a lack of guidance and information about what it is like to go to community college and transfer from there. Therefore, it is in our best interest to provide accurate and useful information to those who struggle with planning for careers or colleges after graduation. </p>
+            <h1 className="vision-title">Our Vision:</h1>
+            <p className="title-vision-text">We plan to create a service that will function as an interactive quiz that will aggregate the data into our database. This data will then be visualized using charts, viewable only by the sponsor and other administrators for our initial version. This initial version will be completed by the end of this course and will be demoed to the faculty and our sponsor. We aim for the initial software to be deployed and tested by the Long Beach Unified School District. We will provide them with data to help educate students on the variety of career paths outside of a four year degree.</p>
+          </div>
+        </section>
+        <section className="team-container">
+          <div className="team-title-container">
+            <h1 className="title-text-team">Meet the team</h1>
+          </div>
+          <div className="sponsor-card">
+            <div className="sponsor-card__image">
+              <img className="sponsor-image" src="/hai.jpg" alt="Photo of Hai"/>
+            </div>
+            <h2 className="sponsor-card__title">Founder: Hai Truong</h2>
+            <div className="sponsor-card__text">
+              <p> Hai has spent the past 8 years as a marketer, copywriter, and more recently a podcast producer. Currently a marketing strategist at UC Irvine, my alma mater, for the Office of the Vice Provost for Teaching and Learning. Hai supports a variety of projects centered on student success, teaching excellence, and also manage an annual $100,000 marketing budget for the university’s summer session division which enrolls approximately 14,000 students each summer, generating millions of dollars in essential revenue that supports staff, faculty, and student-facing programs and resources.</p>
+            </div>
+          </div>
 
-      <div className="about-container">
-          <section className="about-container__journey">
-              <h1 className="about-title">About Journeymxn</h1>
-              <p className = "journey-info">
-              Journeymxn is a web-based platform that aggregates up-to-date labor trend data, vocational/trade/skill-based career programs, and matches student profiles that are generated after an assessment quiz with a comprehensive roadmap that can use to explore different options.
-              </p>
-          </section>
+          <hr className="about__break-line"></hr>
 
-          <section className="about-container__team">
-              <h2 className="about-title team-title">
-                  Meet the team
-              </h2>
-              <div className="sponsor about-flex">
-                  <div className="sponsor-profile person-profile">
-                  <h2 className ="sponsor__title person-title">Founder: Hai Truong</h2>
-                    <img src="/Hai.jpg" alt="photo of hai"  className="person-photo" />
-                  </div>
-                  <p className="person-text">
-                    Hai Truong is an experience marketer, copywriter, and podcast producer, whose projects primarily focus on student success and teaching excellence.
-                  </p>
-              </div>
-              <div className="team-developers">
-                  <div className="personOne about-flex">
-                    <p className="person-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                    <div className="person-profile">
-                      <h2 className="team-developers__person person-title">Lead Developer: Justin Kang</h2>
-                      <img src="/Justin.jpg" alt="photo of hai" className="person-photo" />
-                      </div>
-                  </div>
-                  <div className="personTwo about-flex">
-                    <div className="person-profile">
-                      <h2 className="team-developers__person person-title">Back-End Developer: Mugdhaa Patankar</h2>
-                      <img src="/Mugdha.jpg" alt="photo of mugdha" className="person-photo"/>
-                    </div>
-                    <p className="person-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                  </div>
-                  <div className="personThree about-flex">
-                    <p className="person-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                    <div className="person-profile">
-                      <h2 className="team-developers__person person-title">Back-End: Gabe De Mesa</h2>
-                      <img src="/Gabe.jpg" alt="photo of hai" className="person-photo" />
-                      </div>
-                  </div>
-                  <div className="personFour about-flex">
-                    <div className="person-profile">
-                      <h2 className="team-developers__person person-title">Front-End Developer: Patrick Castillofuentes</h2>
-                      <img src="/Patrick.jpg" alt="photo of mugdha" className="person-photo"/>
-                    </div>
-                    <p className="person-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                  </div>
+          <div className="team-card">
+            <div className="team-card__image-left">
+              <img className="team-image-left" src="/justin.jpg" alt="Photo of Justin"/>
+            </div>
+            <h2 className="team-card__title-left">Development Team Leader/ Front-End Developer: Justin Kang</h2>
+            <div className="team-card__text-left">
+              <p>Justin is a 4th year Computer Science Major with a minor in Statistics. Justin specializes in Intelligent Systems, Machine Learning, and Creation/Analysis of Statistical Models.</p>
+            </div>
+          
+            <div className="team-card__image-right">
+              <img className="team-image-right" src="/mugdha.jpg" alt="Photo of Mugdhaa"/>
+            </div>
+            <h2 className="team-card__title-right">Back-End Developer: Mugdhaa Patankar</h2>
+            <div className="team-card__text-right">
+              <p>Mugdhaa is a 3rd year Computer Science Major with a specialization in Information. An experienced web developer, who’s strengths shine in Back-End Development.</p>
+            </div>
+          </div>
+          <div className="team-card">
+            <div className="team-card__image-left">
+              <img className="team-image-left" src="/gabe.jpg" alt="Photo of Gabe"/>
+            </div>
+            <h2 className="team-card__title-left">Back-End Developer: Gabe De Mesa</h2>
+            <div className="team-card__text-left">
+              <p>Gabe is a 3rd year Computer Science Major, who enjoys working with data bases and big data, also specializes in Information.</p>
+            </div>
+          
+            <div className="team-card__image-right">
+              <img className="team-image-right" src="/patrick.jpg" alt="Photo of Patrick"/>
+            </div>
+            <h2 className="team-card__title-right">Front-End Developer: Patrick Castillofuentes</h2>
+            <div className="team-card__text-right">
+              <p>Patrick is a 5th Year Computer Science Major. He specializes in network security and cryptography, in addition to having background in graphic design.  </p>
+            </div>
+          </div>
+          
+        </section>
+      </article>
 
-
-              </div>
-          </section>
-
-      </div>
-  </div>
+    </div>
 
   )
 }
