3695ef49fd87d8337f5ea3a3cd27b7d7bc4c879c
Patrick Castillofuentes <ptcastil@uci.edu>
2021-04-23 00:05:42 -0700

    added icon images for the login page and started to change styles

--- pages/login.js
+++ pages/login.js
@@ -7,18 +7,20 @@ import styles from '../styles/pages/login.module.scss'
 export default function Login() {
   return (
     <div className={styles["container"]}>
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+      <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text&display=swap" rel="stylesheet"/> 
       <Head>
         <title>Journeymxn</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    
+        <div className={styles["logo"]}>
+            <img src="/../public/account_circle_black_24dp.svg" alt="journeymxn-logo"/>
+            <h1>journeymxn</h1>
+        </div>
         <form action="POST" className={styles["card"]}>
             <h1 className={styles["title"]}>Login</h1>
 
-            <label for="uname" className={styles["userLabel"]}>Username</label>
             <input type="text" placeholder="Enter Username" name="uname" required className={styles["userInput"]}/>
-            
-            <label for="pass" className={styles["passLabel"]}>Password</label>
             <input type="password" placeholder="Enter Password" name="pass" required className={styles["passInput"]}/>
 
             <button type="submit" className={styles["submit"]}>LOGIN</button>
