fdc31615a40859057d3d369e73cba2e3e318e9e9
Patrick Castillofuentes <ptcastil@uci.edu>
2021-04-28 18:52:24 -0700

    removed focus stle change

--- components/quiz_form.js
+++ components/quiz_form.js
@@ -6,12 +6,21 @@ import styles from '../styles/components/quiz_form.module.scss'
 function makeAnswerChoicesHTML(answers, question_num) {
 	return answers.map((answer, index) => {
 		return (
-			<div class={styles["choice__radio-group"]}>
+			<div className={styles["choice__radio-group"]}>
 				<input type="radio" id={`q${question_num + 1}a${index + 1}`} name={`choice${question_num + 1}`} className={styles["choice__radio-btn"]} />
 				<label for={`q${question_num + 1}a${index + 1}`} className={styles["choice__label"]}>{answer}</label>
 			</div>
 		)
 	})
+}
+
+function makeQuizzes(questions) {
+	questions.forEach((qna_pair, index) => {
+		let question = qna_pair["question"];
+		let answers = qna_pair["answers"];
+
+		let htmlDOM = 1;
+	});
 }
 
 export default function QuizForm({
@@ -21,20 +30,30 @@ export default function QuizForm({
 				  "answers": ["answer1", "answer2", "answer3"]}]
 }) {
 	return (
-		<form action="POST" className={styles["form"]}>
-			{questions.map((each, index) => {
-				return  (
-					<div className="quiz">
-						<div className={styles["question"]}>
-							{each.question}
-						</div>
-						{makeAnswerChoicesHTML(each.answers, index)}
-					</div>
-				);
-			})}
+		<div className={styles["container"]}>
 
-			<button type="submit" className={styles["quiz__submit-btn"]}>Submit</button>
-		</form>
-		
+			<ul id="quiz" className={styles["slide-container"]}>
+				<li id="slide" className={styles["quiz-slide"]}>
+					<div class={`${styles["quiz-slide__content"]} ${styles["quiz-slide__content--1"]}`}>
+						<span>Lorem</span>
+					</div>    
+				</li>
+				<li id="slide" className={styles["quiz-slide"]}>
+					<div class={`${styles["quiz-slide__content"]} ${styles["quiz-slide__content--2"]}`}>
+						<span>Ipsum</span>
+					</div>    
+				</li>
+				<li id="slide" className={styles["quiz-slide"]}>
+					<div class={`${styles["quiz-slide__content"]} ${styles["quiz-slide__content--3"]}`}>
+						<span>Spani</span>
+					</div>    
+				</li>
+			</ul>
+
+			<span className={`${styles["nav-btn"]} ${styles["nav-btn__left"]} fa fa-chevron-left fa-3x`} id="left-btn"></span>
+			<span className={`${styles["nav-btn"]} ${styles["nav-btn__right"]} fa fa-chevron-right fa-3x`} id="right-btn"></span>
+			
+      	</div>
+
 	);
 }
