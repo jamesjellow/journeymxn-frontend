1630dcc5371bfef8a8fbd498225db48772fe4bd1
Cynmil <hyunkok1@uci.edu>
2021-04-29 13:32:24 -0700

    Finish the home header layout

--- components/navigation.js
+++ components/navigation.js
@@ -5,34 +5,50 @@ import styles from '../styles/components/navigation.module.scss'
 export default function Navigation() {
     return (
         <nav className={styles["nav-bar"]}>
-            <img src="/icon-256.png" alt="journeymxn logo" className={styles["small-logo"]} />
+            <Link href="/">
+                <a href="#!" className={styles["large-logo--link"]}>
+                    <img src="/icon-full.png" alt="journeymxn logo" className={styles["large-logo"]} />
+                </a>
+            </Link>
 
             <ul className={styles["nav"]}>
                 <Link href="/">
                     <li className={styles["nav__item"]}>
+                        <svg className={styles["nav__icon"]} id="#home-nav" >
+                            <use xlinkHref="/sprite.svg#icon-check_box" />
+                        </svg>
                         <a href="#!" className={styles["nav__link"]}>
-                            <span>Home</span>
+                            <span>home</span>
                         </a>
                     </li>
                 </Link>
                 <Link href="/quiz">
 					<li className={styles["nav__item"]}>
+                        <svg className={styles["nav__icon"]}>
+                            <use xlinkHref="/sprite.svg#icon-check_box_outline_blank" />
+                        </svg>
                         <a href="#!" className={styles["nav__link"]}>
-                            <span>Quiz</span>
+                            <span>quiz</span>
                         </a>
                     </li>
                 </Link>
                 <Link href="/admin">
 					<li className={styles["nav__item"]}>
+                        <svg className={styles["nav__icon"]}>
+                            <use xlinkHref="/sprite.svg#icon-check_box_outline_blank"></use>
+                        </svg>
                         <a href="#!" className={styles["nav__link"]}>
-                            <span>Admin</span>
+                            <span>admin</span>
                         </a>
                     </li>
                 </Link>
                 <Link href="/about">
 					<li className={styles["nav__item"]}>
+                        <svg className={styles["nav__icon"]}>
+                            <use xlinkHref="/sprite.svg#icon-check_box_outline_blank"></use>
+                        </svg>
                         <a href="#!" className={styles["nav__link"]}>
-                            <span>About Us</span>
+                            <span>about</span>
                         </a>
                     </li>
                 </Link>
--- pages/index.js
+++ pages/index.js
@@ -18,19 +18,29 @@ export default function Home() {
 
       <NavComponent />
       
-      <header className={styles["home-header"]}>
-        <div className={styles["home-header__heading-container"]}>
-            <h1 className={styles["heading-primary"]}>
-                <span className={`${styles["heading-primary--main"]} u-margin-bottom-small`}>Journeymxn</span>
-                <span className={styles["heading-primary--sub"]}>Discover Your</span>
-                <span className={styles["heading-primary--sub"]}>Colleges. Careers. Futures.</span>
-            </h1>
-
-            <Link href="/quiz">
-              <a href="#!" className={`${btnStyles["btn"]} ${btnStyles["btn--white"]} ${btnStyles["btn--animated"]}`}>Take Our Career Quiz</a>
-            </Link>
+      <header className={styles["header"]}>
+        <div className={styles["header__heading-container"]}>
+          <h1 className={styles["header__heading-primary--main"]}>
+            Let us help your career find you
+          </h1>
+          <h1 className={styles["header__heading-primary--sub"]}>
+            Take our short assessment and explore the in-demand careers and programs personalized for you.
+          </h1>
+          <Link href="/quiz">
+            <a href="#!" className={`${btnStyles["btn"]} ${btnStyles["btn--black"]} ${btnStyles["btn--animated"]}`}>Take Our Career Quiz</a>
+          </Link>
         </div>
+
+        
+        <div className={styles["header__img-wrapper"]}>
+          <img src="/header_background2.jpg" className={styles["header__img"]} alt="header-img"/>
+        </div>
+        
       </header>
+
+      <main className={styles["home-header"]}>
+
+      </main>
     </div>
   )
 }
