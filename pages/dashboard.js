dc393499ee8de2354f7619ba9ef0815f2041cb9b
Patrick Castillofuentes <ptcastil@uci.edu>
2021-05-06 16:36:15 -0700

    changed dashboard to be simpler

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,144 +1,8 @@
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
 import styles from '../styles/pages/dashboard.module.scss'
-const Dropdown = dynamic(() => import('../components/dropdown'))
 
-const industries = [
-  {
-    id: 1,
-    value: 'Finance',
-  },
-  {
-    id: 2,
-    value: 'Business/Administration',
-  },
-  {
-    id: 3,
-    value: 'Creative/Media',
-  },
-  {
-    id: 4,
-    value: 'Education/Public Sector',
-  },
-  {
-    id: 5,
-    value: 'Healthcare',
-  },
-  {
-    id: 6,
-    value: 'Technology',
-  },
-  {
-    id: 7,
-    value: 'Service',
-  },
-  {
-    id: 8,
-    value: 'Skilled Trade',
-  },
-  {
-    id: 9,
-    value: 'Architecture/Engineering',
-  },
-  {
-    id: 10,
-    value: 'Public Service',
-  },
-];
-const skills = [
-  {
-    id: 1,
-    value: 'Presentation'
-  },
-  {
-    id: 2,
-    value: 'Critical thinking'
-  },
-  {
-    id: 3,
-    value: 'Communication'
-  },
-  {
-    id: 4,
-    value: 'Negotiation'
-  },
-  {
-    id: 5,
-    value: 'Multitasking'
-  },
-  {
-    id: 6,
-    value: 'Inductive reasoning'
-  },
-  {
-    id: 7,
-    value: 'Ability to work under stress/pressure'
-  },
-  {
-    id: 8,
-    value: 'Problem solving'
-  },
-  {
-    id: 9,
-    value: 'Flexibility'
-  },
-  {
-    id: 10,
-    value: 'Creativity'
-  },
-  {
-    id: 11,
-    value: 'Interpersonal'
-  },
-  {
-    id: 12,
-    value: 'Analytical'
-  },
-  {
-    id: 13,
-    value: 'Public speaking'
-  },
-  {
-    id: 14,
-    value: 'Conflict management'
-  },
-  {
-    id: 15,
-    value: 'Reasoning'
-  },
-  {
-    id: 16,
-    value: 'Persuasion'
-  },
-  {
-    id: 17,
-    value: 'Decision making'
-  },
-  {
-    id: 18,
-    value: 'Negotiation'
-  },
-  {
-    id: 19,
-    value: 'Detail Oriented'
-  },
-  {
-    id: 20,
-    value: 'Physical strength'
-  },
-  {
-    id: 21,
-    value: 'Basic math skills'
-  },
-  {
-    id: 22,
-    value: 'Coordination'
-  },
-  {
-    id: 23,
-    value: 'Logical thinking'
-  }
-]
+const Dropdown = dynamic(() => import('../components/dropdown'))
 
 export default function Dashboard() {
   return (
@@ -147,18 +11,74 @@ export default function Dashboard() {
         <title>Journeymxn</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <header className={styles["bar"]}>
+        <img src="/icon-256.png" alt="journeymxn-logo" className={styles["icon-logo"]}/>
+        <h1>Dashboard</h1>
+        <a href="" className={styles["signout"]}>
+          Sign out
+        </a>
+      </header>
 
-      <section className={styles["dash"]}>
-        <div className={styles["sidebar"]}>
-          <Dropdown title="Industries" items = {industries} multiSelect/>
-          <Dropdown title="Skill" items = {skills} multiSelect/>
-          <input>
-            
-          </input>
+      <section className={styles["filters"]}>
+        <h1>Filter By: </h1>
+        <h1>Industry</h1>
+        <select name="industries" id="industries">
+          <option value="none" selected disabled hidden>
+            Choose an Industry
+          </option>
+          <option value="Finance">Finance</option>
+          <option value="Business/Administration">Business/Administration</option>
+          <option value="Creative/Media">Creative/Media</option>
+          <option value="Education/Public Sector">Education/Public Sector</option>
+          <option value="Healthcare">Healthcare</option>
+          <option value="Technology">Technology</option>
+          <option value="Service">Skilled</option>
+          <option value="Skilled Trade">Skilled Trade</option>
+          <option value="Architecture and Engineering">Architecture and Engineering</option>
+          <option value="Public Service">Public Service</option>
+        </select>
+        <h1>Skillset</h1>
+        <select name="SkillSets" id="skillsets">
+        <option value="none" selected disabled hidden>
+            Choose an SkillSet
+          </option>
+          <option value="Presentation">Presentation</option>
+          <option value="Critical thinking">Critical thinking</option>
+          <option value="Communication">Communication</option>
+          <option value="Negotiation">Negotiation</option>
+          <option value="Multitasking">Multitasking</option>
+          <option value="Inductive reasoning">Inductive reasoning</option>
+          <option value="Ability to work under stress/pressure">Ability to work under stress/pressure</option>
+          <option value="Problem solving">FinanProblem solvingce</option>
+          <option value="Flexibility">Flexibility</option>
+          <option value="Creativity">Creativity</option>
+          <option value="Interpersonal">Interpersonal</option>
+          <option value="Analytical">Analytical</option>
+          <option value="Public speaking">Public speaking</option>
+          <option value="Conflict management">Conflict management</option>
+          <option value="Reasoning">Reasoning</option>
+          <option value="Persuasion">Persuasion</option>
+          <option value="Decision making">Decision making</option>
+          <option value="Negotiation">Negotiation</option>
+          <option value="Detail Oriented">Detail Oriented</option>
+          <option value="Physical strength">Physical strength</option>
+          <option value="Basic math skills">Basic math skills</option>
+          <option value="Coordination">Coordination</option>
+          <option value="Logical thinking">Logical thinking</option>
+        </select>
+        <h1>Zip Code: </h1>
+        <input type="text" placeholder="Enter ZipCode" name="uname" required className={styles["zip_input"]}/>
+        <input className={styles["submit"]} type="submit" value="Submit"/>
+      </section>
+      <section className={styles["data"]}>
+        <div className={styles["chart"]}>
         </div>
-        <div className={styles["content"]}>
-          hello
-        </div>   
+        <div className={styles["graph"]}>
+        </div>
+        <div className={styles["graph-2"]}>
+        </div>
+        <div className={styles["sheet"]}>
+        </div>
       </section>
     </div>
   )
