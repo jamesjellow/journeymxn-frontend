4d828045c5e4cac52d9cd370f59cf110c7e8141a
Patrick Castillofuentes <ptcastil@uci.edu>
2021-05-21 23:36:51 -0700

    enabled filtering of charts

--- pages/admin.js
+++ pages/admin.js
@@ -1,54 +1,37 @@
 import Head from 'next/head'
-import dynamic from 'next/dynamic'
-import {useEffect} from 'react'
+
+import {useState, useEffect} from 'react'
 import EmbedSDK from '@mongodb-js/charts-embed-dom'
 
-import {useState} from '../components/context'
 import styles from '../styles/pages/admin.module.scss'
 
 export default function Admin() {
-  const state = useState();
 
-  //CHECK IF LOGGED IN
-  //CHANGE AFTER LOGIN IS SETUP
-  useEffect( () => {
-    if(state.is_login) {
-      window.location.href = "/login"
-    }
-  }, [])
+  const sdk = new EmbedSDK({
+    baseUrl: 'https://charts.mongodb.com/charts-data-analytics-platform-gqxba'
+  })
 
-  //RENDER IF NOT LOGGED IN
-  if(state.is_login) {
-    return (
-     <div className={styles["redirect"]}>
-      <img src="/icon-256.png" alt="journeymxn-logo" className={styles["icon-logo"]}/>
-      <h1>Please login to view this page!</h1>
-      <h1>Redirecting to Login</h1>
-     </div> 
-    )
-  }
+  const submissions = sdk.createChart({chartId: '93db39e7-506c-4fd4-a9ca-1fe5448217c8', width: 600, height: 400})
+  const countBySchool = sdk.createChart({chartId: '38226042-9d91-488c-b999-c9174c66c671', width: 800, height: 400})
+  const recByIndustry = sdk.createChart({chartId: '17f9410e-9387-4684-858a-71f6d8a56b6d', width: 800, height: 400})
+  const recBySchool = sdk.createChart({chartId: 'd007be5e-ab94-4856-a659-96ff50ad812d', width: 600, height: 400})
+
 
   function renderCharts() {
-    const sdk = new EmbedSDK({
-        baseUrl: 'https://charts.mongodb.com/charts-data-analytics-platform-gqxba'
-    })
-    
-    const submissions = sdk.createChart({chartId: '93db39e7-506c-4fd4-a9ca-1fe5448217c8', width: 600, height: 400})
-    const countBySchool = sdk.createChart({chartId: '38226042-9d91-488c-b999-c9174c66c671', width: 800, height: 400})
-    const recByIndustry = sdk.createChart({chartId: '17f9410e-9387-4684-858a-71f6d8a56b6d', width: 800, height: 400})
-    const recBySchool = sdk.createChart({chartId: 'd007be5e-ab94-4856-a659-96ff50ad812d', width: 600, height: 400})
-
     submissions.render(document.getElementById('submissions'))
     countBySchool.render(document.getElementById('countBySchool'))
     recByIndustry.render(document.getElementById('recByIndustry'))
     recBySchool.render(document.getElementById('recBySchool'))
-  } 
-  
+  }
+
+  function filterCharts(value) {
+    submissions.setFilter({school_name: (value)})
+    recByIndustry.setFilter({school_name: (value)})
+  }
 
   useEffect(()=> {
     renderCharts()
   }, [])
-
 
   return (
     <div className={styles["container"]}>
@@ -74,10 +57,19 @@ export default function Admin() {
             <option value="Marina High">Marina High School</option>
             <option value="Ocean View High">Ocean View High School</option>
             <option value="Westminster High">Westminster High School</option>
-            <option value="Valley Vista High (Continuaiton)">Valley Vista High School</option>
+            <option value="Valley Vista High (Continuation)">Valley Vista High School</option>
             <option value="Huntington Beach Adult School">Huntington Beach Adult School</option>
             <option value="Coast High">Coast High School</option>
           </select>
+          <button onClick={
+            () => {
+              const name = document.getElementById('school').value
+              console.log()
+              if(name){
+                filterCharts(name)
+              }
+            }
+          }>Click</button>
         </div>
       </section>        
       <section className={styles["dash"]}>
