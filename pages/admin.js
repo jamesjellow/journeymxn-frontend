f9c1b2cb6090288ca4f5eba72a7323c6d7e4bbbd
castillofu <44993467+castillofu@users.noreply.github.com>
2021-05-26 18:43:59 -0700

    Dashboard (#25)
    * add SDK and embed charts on admin
    
    * moved location of charts
    
    * enabled filtering of charts
    
    * added All Schools to filter, & added change styles
    
    * testing auth

--- pages/admin.js
+++ pages/admin.js
@@ -1,22 +1,107 @@
 import Head from 'next/head'
-import dynamic from 'next/dynamic'
-import Link from 'next/link'
 
-const NavComponent = dynamic(() => import('../components/navigation'))
+import {useState, useEffect} from 'react'
+import EmbedSDK from '@mongodb-js/charts-embed-dom'
 
-export default function Quiz() {
+import styles from '../styles/pages/admin.module.scss'
+
+export default function Admin() {
+
+  const sdk = new EmbedSDK({
+    baseUrl: 'https://charts.mongodb.com/charts-data-analytics-platform-gqxba'
+  })
+
+  const submissions = sdk.createChart({chartId: '93db39e7-506c-4fd4-a9ca-1fe5448217c8', width: 600, height: 400})
+  const countBySchool = sdk.createChart({chartId: '38226042-9d91-488c-b999-c9174c66c671', width: 800, height: 400})
+  const recByIndustry = sdk.createChart({chartId: '17f9410e-9387-4684-858a-71f6d8a56b6d', width: 800, height: 400})
+  const recBySchool = sdk.createChart({chartId: 'd007be5e-ab94-4856-a659-96ff50ad812d', width: 600, height: 400})
+  const topSkills = sdk.createChart({chartId: '83caba7d-6f88-444f-896f-bc95ae885e72', width: 600, height: 400})
+  const avgScoreSkills = sdk.createChart({chartId: 'c490d63f-4716-4d33-84b1-06f3b68dcfc8', width: 600, height: 400})
+
+  function renderCharts() {
+    submissions.render(document.getElementById('submissions'))
+    countBySchool.render(document.getElementById('countBySchool'))
+    recByIndustry.render(document.getElementById('recByIndustry'))
+    recBySchool.render(document.getElementById('recBySchool'))
+    topSkills.render(document.getElementById('topSkills'))
+    avgScoreSkills.render(document.getElementById('avgScoreSkills'))
+  }
+  
+
+  function filterCharts(value) {
+    submissions.setFilter({school_name: value})
+    countBySchool.setFilter({school_name: value})
+    recByIndustry.setFilter({school_name: value})
+    recBySchool.setFilter({'school_name': value})
+    topSkills.setFilter({ 'form_response.school_name': value})
+    avgScoreSkills.setFilter({ 'form_response.school_name': value})
+  }
+
+  function clearFilter(){
+    submissions.setFilter({})
+    countBySchool.setFilter({})
+    recByIndustry.setFilter({})
+    recBySchool.setFilter({})
+    topSkills.setFilter({})
+    avgScoreSkills.setFilter({})
+  }
+
+  useEffect(()=> {
+    renderCharts()
+  }, [])
+
   return (
-    <div className="container">
+    <div className={styles["container"]}>
       <Head>
         <title>Journeymxn</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <header className={styles["bar"]}>
+        <img src="/icon-256.png" alt="journeymxn-logo" className={styles["icon-logo"]}/>
+        <h1>Dashboard</h1>
+        <a href="" className={styles["signout"]}>
+          Sign out
+        </a>
+      </header>
+      <section className={styles["filters"]}>
+        <h1>Filter By: </h1>
+        <div className={styles["nav"]}>
+          <select name="school" id="school" className={styles["dropdown"]}>
+            <option value="All Schools" defaultValue>All Schools</option>
+            <option value="Edison High">Edison High School</option>
+            <option value="Fountain Valley High">Fountain Valley High School</option>
+            <option value="Huntington Beach High">Huntington Beach High School</option>
+            <option value="Marina High">Marina High School</option>
+            <option value="Ocean View High">Ocean View High School</option>
+            <option value="Westminster High">Westminster High School</option>
+            <option value="Valley Vista High (Continuation)">Valley Vista High School</option>
+            <option value="Huntington Beach Adult School">Huntington Beach Adult School</option>
+            <option value="Coast High">Coast High School</option>
+          </select>
+        </div>
+        <button className={styles["button"]}onClick={
+            () => {
+              const name = document.getElementById('school').value
+              console.log()
+              if(name=="All Schools"){
+                clearFilter()
+              }
+              else if(name)
+              {
+                filterCharts(name)
+              }
+            }
+          }>Filter</button>
+      </section>        
+      <section className={styles["dash"]}>
+            <div className={styles["chart"]} id="submissions"></div>
+            <div className={styles["chart"]} id="countBySchool"></div>
+            <div className={styles["chart"]} id="recByIndustry"></div>
+            <div className={styles["chart"]} id="recBySchool"></div>
+            <div className={styles["chart"]} id="topSkills"></div>
+            <div className={styles["chart"]} id="avgScoreSkills"></div>
+      </section>
 
-      <NavComponent />
-      
-      <div>
-        <h2>Admin</h2>
-      </div>
     </div>
   )
 }
--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,50 @@
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
 import Link from 'next/link'
+import axios from "axios"
+
+import {useEffect} from 'react'
+import {useState} from '../components/context'
 
 import styles from '../styles/pages/login.module.scss'
 
 export default function Login() {
+
+  const state = useState();
+
+  const submit = async (e) => {
+
+    e.preventDefault()
+    
+    let email = document.querySelector("#email").value
+    let password = document.querySelector("#pass").value
+
+    let submisison = {
+      "email": email,
+      "password": password
+    }
+
+    const url = 'https://journeymxn-api.herokuapp.com/login';
+    
+    try {
+        const res = await fetch(url, {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(submisison)
+        })
+
+        console.log(res)
+
+        if(res.status == 200) {
+          window.location.href = "/admin"
+        }
+    } catch (e) {
+      console.log(e)
+    }
+  } 
+
   return (
     <div className={styles["container"]}>
       <link rel="preconnect" href="https://fonts.gstatic.com"/>
@@ -17,20 +57,22 @@ export default function Login() {
             <img src="/icon-256.png" alt="journeymxn-logo" className={styles["icon-logo"]}/>
             <h1>journeymxn</h1>
         </div>
-        <form action="POST" className={styles["card"]}>
+        <form action="POST" className={styles["card"]} onSubmit={submit}>
             <h1 className={styles["title"]}>Login</h1>
 
             <div className={styles["input_pair"]}>
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM7.07 18.28c.43-.9 3.05-1.78 4.93-1.78s4.51.88 4.93 1.78C15.57 19.36 13.86 20 12 20s-3.57-.64-4.93-1.72zm11.29-1.45c-1.43-1.74-4.9-2.33-6.36-2.33s-4.93.59-6.36 2.33C4.62 15.49 4 13.82 4 12c0-4.41 3.59-8 8-8s8 3.59 8 8c0 1.82-.62 3.49-1.64 4.83zM12 6c-1.94 0-3.5 1.56-3.5 3.5S10.06 13 12 13s3.5-1.56 3.5-3.5S13.94 6 12 6zm0 5c-.83 0-1.5-.67-1.5-1.5S11.17 8 12 8s1.5.67 1.5 1.5S12.83 11 12 11z"/></svg>
-              <input type="text" placeholder="Enter Username" name="uname" required className={styles["userInput"]}/>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+              <path d="M0 0h24v24H0V0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM7.07 18.28c.43-.9 3.05-1.78 4.93-1.78s4.51.88 4.93 1.78C15.57 19.36 13.86 20 12 20s-3.57-.64-4.93-1.72zm11.29-1.45c-1.43-1.74-4.9-2.33-6.36-2.33s-4.93.59-6.36 2.33C4.62 15.49 4 13.82 4 12c0-4.41 3.59-8 8-8s8 3.59 8 8c0 1.82-.62 3.49-1.64 4.83zM12 6c-1.94 0-3.5 1.56-3.5 3.5S10.06 13 12 13s3.5-1.56 3.5-3.5S13.94 6 12 6zm0 5c-.83 0-1.5-.67-1.5-1.5S11.17 8 12 8s1.5.67 1.5 1.5S12.83 11 12 11z"/></svg>
+              <input id="email" type="email" placeholder="Enter Email" name="uname" required className={styles["userInput"]}/>
             </div>
 
             <div className={styles["input_pair"]}>
-              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g fill="none"><path d="M0 0h24v24H0V0z"/><path d="M0 0h24v24H0V0z" opacity=".87"/></g><path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"/></svg>
-              <input type="password" placeholder="Enter Password" name="pass" required className={styles["passInput"]}/>
+              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+                <g fill="none"><path d="M0 0h24v24H0V0z"/><path d="M0 0h24v24H0V0z" opacity=".87"/></g><path d="M18 8h-1V6c0-2.76-2.24-5-5-5S7 3.24 7 6v2H6c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V10c0-1.1-.9-2-2-2zM9 6c0-1.66 1.34-3 3-3s3 1.34 3 3v2H9V6zm9 14H6V10h12v10zm-6-3c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"/></svg>
+              <input id="pass" type="password" placeholder="Enter Password" name="pass" required className={styles["passInput"]}/>
             </div>
 
-            <button type="submit" className={styles["submit"]}>Login</button>
+            <button type="submit" className={styles["submit"]} >Login</button>
         </form>
     </div>
   )
