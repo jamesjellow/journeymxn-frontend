25a29cb9b9260593a444ef1cdfca5dd95fb5ebd7
castillofu <44993467+castillofu@users.noreply.github.com>
2021-06-02 18:36:31 -0700

    Dashboard (#26)
    * add SDK and embed charts on admin
    
    * moved location of charts
    
    * enabled filtering of charts
    
    * added All Schools to filter, & added change styles
    
    * testing auth
    
    * JWT implementation
    
    * Added message for users when login credentials fail
    
    * added loading animation on fetch
    
    * finalized implementaiton of user auth

--- pages/admin.js
+++ pages/admin.js
@@ -1,16 +1,22 @@
 import Head from 'next/head'
 
-import {useState, useEffect} from 'react'
+import {useEffect} from 'react'
+import {useState, useDispatchState} from '../components/context'
 import EmbedSDK from '@mongodb-js/charts-embed-dom'
 
 import styles from '../styles/pages/admin.module.scss'
 
 export default function Admin() {
 
+  const state = useState()
+  const dispatch = useDispatchState();
+
+  //SET UP LINK TO ACCESS CHARTS
   const sdk = new EmbedSDK({
     baseUrl: 'https://charts.mongodb.com/charts-data-analytics-platform-gqxba'
   })
 
+  //CREATE CHARTS
   const submissions = sdk.createChart({chartId: '93db39e7-506c-4fd4-a9ca-1fe5448217c8', width: 600, height: 400})
   const countBySchool = sdk.createChart({chartId: '38226042-9d91-488c-b999-c9174c66c671', width: 800, height: 400})
   const recByIndustry = sdk.createChart({chartId: '17f9410e-9387-4684-858a-71f6d8a56b6d', width: 800, height: 400})
@@ -26,7 +32,6 @@ export default function Admin() {
     topSkills.render(document.getElementById('topSkills'))
     avgScoreSkills.render(document.getElementById('avgScoreSkills'))
   }
-  
 
   function filterCharts(value) {
     submissions.setFilter({school_name: value})
@@ -46,22 +51,54 @@ export default function Admin() {
     avgScoreSkills.setFilter({})
   }
 
+  const checkJWT = async () => {
+    const url = 'https://journeymxn-api.herokuapp.com/admin';
+    try {
+        const res = await fetch(url, {
+          method: "get",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${localStorage.getItem("token")}`
+          }
+        })
+
+        if(res.status == 200) {
+          renderCharts()
+        }
+        else {
+          window.location.href = "/login"
+        }
+
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const logOut = () => {
+    localStorage.removeItem('token')
+    dispatch({type: "LOGOUT", is_login: true})
+    window.location.href="/"
+  }
+
+  //RENDER INITIAL CHARTS
   useEffect(()=> {
-    renderCharts()
+    checkJWT()
   }, [])
 
   return (
-    <div className={styles["container"]}>
+    <div className={styles["container"]} id="expose">
       <Head>
         <title>Journeymxn</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className={styles["bar"]}>
-        <img src="/icon-256.png" alt="journeymxn-logo" className={styles["icon-logo"]}/>
-        <h1>Dashboard</h1>
-        <a href="" className={styles["signout"]}>
-          Sign out
+      <header className={styles["bar"]} >
+        <a href="/" className={styles["icon-logo"]}>
+          <img src="/icon-256.png" alt="journeymxn-logo" />
         </a>
+        <h1>Dashboard</h1>
+        <button className={styles["signout"]} onClick={logOut}>
+          Log out
+        </button>
       </header>
       <section className={styles["filters"]}>
         <h1>Filter By: </h1>
